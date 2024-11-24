66e94d011ef4d1c93185f1e32d0d8f4208e7c1e8
dattrungnam <datvtrg0510@gmail.com>
2024-11-25 00:36:02 +0700

    feat: get group topic detail with user

--- src/group-topic/group-topic.module.ts
+++ src/group-topic/group-topic.module.ts
@@ -5,9 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupTopic } from './entity/groupTopic.entity';
 import { TagModule } from '../tag/tag.module';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
+import { TopicHistory } from '../topic-history/entity/topicHistory.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GroupTopic]), TagModule, CloudinaryModule],
+  imports: [
+    TypeOrmModule.forFeature([GroupTopic, TopicHistory]),
+    TagModule,
+    CloudinaryModule,
+  ],
   controllers: [GroupTopicController],
   providers: [GroupTopicService],
 })
